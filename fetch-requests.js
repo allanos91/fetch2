96bfd63edb60f00b8d19f7b852fe8cbef1b0b489
Alexander <allanosinhb@gmail.com>
2023-12-12 21:19:32 -0800

    finished

--- fetch-requests.js
+++ fetch-requests.js
@@ -2,25 +2,29 @@
 Make fetch requests in the browser for each of the following tasks.
 Paste your code for fetch requests here once you finish each task.
 */
-
 /* =============== 1. Print the status code of the response =============== */
-
-// Your code here 
-
-
-
+// Your code here
+fetch('/products')
+    .then(res => console.log(res.status));
 /* ====== 2. Print true if the status of the response was successful ====== */
-
-// Your code here 
-
-
-
+// Your code here
+fetch('/products')
+.then(res => console.log(res.ok));
 /* =================== 3. Print the Content-Type Header =================== */
-
-// Your code here 
-
-
-
+// Your code here
+fetch('/products')
+.then(res => console.log(res.headers.get('Content-Type')));
 /* ============== 4. Print the body of the response as text =============== */
-
-// Your code here 
+// Your code here
+fetch('/products')
+.then(res => res.text())
+.then(resBody => console.log(resBody))
+async function getProducts(){
+    const res = await fetch('/products');
+    if(res.ok){
+        const resBody = await res.text();
+        console.log(resBody)
+    } else {
+        console.log(res.status)
+    }
+}
